aade86e6b65816a89f30c66988fe2bfa24e611ff
Ruwanox <133478265+Ruwanox@users.noreply.github.com>
2023-11-23 22:34:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+<script>
+        document.getElementById('prolonged_declarations').addEventListener('click', function() {
+            alert('Prolonged Declarations feature clicked');
+        });
+
+        document.getElementById('histogram_variance').addEventListener('click', function() {
+            
+            alert('Histogram Variance feature clicked');
+        });
+
+        document.getElementById('histogram_mode').addEventListener('click', function() {
+            alert('Histogram Mode feature clicked');
+        });
+
+        document.getElementById('accelerations').addEventListener('click', function() {
+            // Add logic for accelerations feature
+            alert('Accelerations feature clicked');
+        });
+    </script>
